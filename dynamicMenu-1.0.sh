cd0d7364558d4a277542e2842907ccb25bf975f1
Mickey Fox <mike@cmkconsulting.com>
2020-05-29 08:12:59 +0200

    Update dynamicMenu-1.0.sh

--- dynamicMenu-1.0.sh
+++ dynamicMenu-1.0.sh
@@ -9,6 +9,17 @@
 # Last Revision Date 5.29.2020
 
 # dynamicMenu-1.0.sh
+
+# This program is free software: you can redistribute it and/or modify it under the terms
+# of the GNU General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with this program.
+#If not, see <http://www.gnu.org/licenses/>
 ###################################################################
 
 # 5.28.2020 Version 1.0 is fully working.
